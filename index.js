95b08a44b6d55a94f36a503c1ee5419422b2b8bb
Raul Sebastian Carreno Reyes <raul.carreno@wom.co>
2024-11-27 14:54:49 -0500

    :fire: first commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const axios = require('axios');
+
+exports.handler = async (event) => {
+  try {
+    const { path, httpMethod, body } = event;
+
+    // Rutas
+    if (path === '/healthcheck' && httpMethod === 'GET') {
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          status: 'OK',
+          timestamp: new Date().toISOString(),
+        }),
+      };
+    }
+
+    if (path === '/convert' && httpMethod === 'POST') {
+      if (!body) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({ error: 'Invalid request body' }),
+        };
+      }
+
+      const { amount, base_currency, target_currency } = JSON.parse(body);
+
+      if (!amount || !base_currency || !target_currency) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({ error: 'Missing required fields' }),
+        };
+      }
+
+      // Lógica de conversión de monedas
+      const apiKey = '${secrets.API_KEY}'; // Reemplaza con tu API key de exchangerate-api.com
+      const url = `https://v6.exchangerate-api.com/v6/${secrets.API_KEY}/latest/${base_currency}`;
+
+      const response = await axios.get(url);
+      const rates = response.data.conversion_rates;
+
+      if (!rates || !rates[target_currency]) {
+        return {
+          statusCode: 400,
+          body: JSON.stringify({ error: 'Invalid currency code' }),
+        };
+      }
+
+      const convertedAmount = amount * rates[target_currency];
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          amount,
+          base_currency,
+          target_currency,
+          converted_amount: convertedAmount,
+        }),
+      };
+    }
+
+    // Ruta no encontrada
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ error: 'Not Found', message: 'Resource not found' }),
+    };
+  } catch (error) {
+    console.error('Error:', error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    };
+  }
+};
